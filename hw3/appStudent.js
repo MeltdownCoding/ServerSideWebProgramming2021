acdfc47c0ade0c34572d612e76d1edf4eb98820b
KanapongAiamtip <kannaphongi62@nu.ac.th>
2021-07-02 22:20:38 +0700

    hw3

---
+++ hw3/appStudent.js
@@ -0,0 +1,12 @@
+let register = require('./register');
+
+
+let studentArray = []
+let student = {
+    "ID": 1234,
+    "Fullname": "AAA BBBB",
+    "Curriculum": "IT"
+}
+
+register.add();
+register.del();
---
+++ hw3/register/index.js
@@ -0,0 +1,6 @@
+let add = require('./add');
+let del = require('./del');
+module.exports = {
+    add: add, 
+    del: del
+}
