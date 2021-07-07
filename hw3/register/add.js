f3ecf0e22e88bf676df6fe8813e90452ee2a7dbb
KanapongAiamtip <kannaphongi62@nu.ac.th>
2021-07-07 10:47:46 +0700

    fix

--- hw3/register/add.js
+++ hw3/register/add.js
@@ -1,40 +1,6 @@
-let register = function () {
-    let user = [
-        {
-            "ID": 1234,
-            "Fullname": "AAA BBBB",
-            "Curriculum": "IT"
-        }
-    ]
-
-    let userb = {
-        "ID": 1234,
-        "Fullname": "AAA BBBB",
-        "Curriculum": "IT"
-    }
-
-    let userc = {
-        "ID": 1234,
-        "Fullname": "AAA BBBB",
-        "Curriculum": "IT"
-    }
-
-    let userd = {
-        "ID": 1234,
-        "Fullname": "AAA BBBB",
-        "Curriculum": "IT"
-    }
-
-    let usere = {
-        "ID": 1234,
-        "Fullname": "AAA BBBB",
-        "Curriculum": "IT"
-    }
-
-    user.push(userb)
-    user.push(userc)
-    user.push(userd)
-    user.push(usere)
+let add = function (studentArray,student) {
+   studentArray.push(student)
+   return studentArray;
 
 }
-module.exports = register;
+module.exports = add;
